cc6aa2e9e38383d1bedd9e274d7198f62a611c82
Mingyang Li <53138432+Mingyang-Li@users.noreply.github.com>
2020-02-11 09:36:21 +1300

    Added new functionality
    Add a function to make sure error message is displayed saying "...field is required" as long as the user didn't type in any input, whereas the previous code just displayed error messages were only telling the users about the input length requirements even if the users didn't type in any input.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const form = document.getElementById("form");
 const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const password2 = document.getElementById("password2");
+const form = document.getElementById("form");
 
 //show error message
 function showError(input, message) {
@@ -12,14 +12,40 @@ function showError(input, message) {
     small.innerText = message;
 }
 
-//show success message
+// show success
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = "form-control success";
 }
 
-//check email is valid
-function checkEmail(input) {
+//get fieldname
+function getFieldName(input) {
+    idStr = input.id;
+    return idStr.charAt(0).toUpperCase() + idStr.slice(1);
+}
+
+//check if required fields are filled
+function checkRequired(inputArr) {
+    inputArr.forEach(function (input) {
+        if (input.value.trim() === "") {
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    })
+}
+
+//check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be more than ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be no more than ${max} characters`);
+    }
+}
+
+//check if email is valid
+function checkValidEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
         showSuccess(input);
@@ -28,46 +54,27 @@ function checkEmail(input) {
     }
 }
 
-//check required fields
-function checkRequired(inputArr) {
-    inputArr.forEach(function (input) {
-        if (input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`);
-        } else {
-            showSuccess(input);
-        }
-    });
-}
-
-//check input length
-function checkLength(input, min, max) {
-    if (input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be over ${min} characters`);
-    } else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
-    } else {
-        showSuccess(input)
-    }
-}
-//check password match
-function checkPasswordMatch(input1, input2) {
-    if (input1.value !== input2.value) {
-        showError(input2, "Passwords do not match")
-    }
-}
-
-//get fieldname
-function getFieldName(input) {
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
-
-//EVENT LISTENERS
+//add event listener
 form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     checkRequired([username, email, password, password2]);
-    checkLength(username, 3, 15);
-    checkLength(password, 6, 15);
-    checkEmail(email);
-    checkPasswordMatch(password, password2)
-}); 
+
+    //only display length validation if inputs aren't empty
+    const formControl = input.parentElement;
+    if (formControl.className === "form-control success") {
+        checkLength(username, 3, 10);
+        checkLength(password, 8, 20);
+    }
+
+    checkValidEmail(email);
+});
+
+
+
+//extra functionalities
+/**
+ * make sure username isn't used as password
+ *
+ *
+ */
